b92bf4e4d61f23b91c07d4eb51c40b7671d0adc4
j7lia17 <juliadoboco070@gmail.com>
2025-05-12 16:48:56 -0300

    Add files via upload
    const / app.get / app.post

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const cadAstro = require ("./cadastrarUsuario")
+const express = require('express') 
+const app = express()
 
-cadAstro ("Mariana", 20);
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Bem-vindo ao servidor Express!")
+})
+
+app.get('/sobre', (req, res) => {
+    res.send("Este é um projeto de exemplo com rotas.")
+})
+
+app.get('/contato', (req, res)=> {
+    res.send("Entre em contato pelo e-mail: juliadoboco070@email.com")
+})
+
+app.post('/usuario', (req, res)=> {
+    const nome = req.body.nome
+    const cargo = req.body.cargo
+    res.send("Usuário " + nome + " criado com sucesso! O seu cargo é " + cargo + ".")
+})
+
+
+app.listen(3001,()=>{
+    console.log('servidor rodando em http://localhost:3001')
+})
